3101ed6e1fcac4a87491a2646435b2e3f7f11ad0
tcjiang <oliver8709@gmail.com>
2019-07-20 06:45:10 -0400

    Added clock view
    Added another view of  a clock. Switching by swipe.

--- app/src/main/java/com/example/tjclock2/MainActivity.java
+++ app/src/main/java/com/example/tjclock2/MainActivity.java
@@ -1,20 +1,35 @@
 package com.example.tjclock2;
 
 import android.app.Activity;
-import java.util.Timer;
+import android.view.MotionEvent;
+import android.widget.ViewFlipper;
 import android.widget.TextView;
 import android.os.Bundle;
 
 import java.util.Calendar;
+import java.util.Timer;
 import java.util.TimerTask;
 
+import com.iigo.library.ClockHelper;
+import com.iigo.library.ClockView;
+
 public class MainActivity extends Activity  {
+    private ViewFlipper viewFlipper;
+    private float lastX;
+    private ClockView clockView;
+    private ClockHelper clockHelper;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         final TextView timeView = findViewById(R.id.tjClockTextView);
         final TextView ampmView = findViewById(R.id.tjClockAmPmView);
+        viewFlipper = findViewById(R.id.viewflipper);
+
+        clockView = findViewById(R.id.clockView);
+        clockHelper = new ClockHelper(clockView);
+        clockHelper.start();
 
         TimerTask repeatedTask = new TimerTask() {
             public void run() {
@@ -32,5 +47,51 @@ public class MainActivity extends Activity  {
         long delay  = 1000L; // delay 1 sec
         long period = 1000L; // refresh every sec
         timer.scheduleAtFixedRate(repeatedTask, delay, period);
+    }
+
+    // Using the following method, we will handle all screen swaps.
+    public boolean onTouchEvent(MotionEvent touchevent) {
+        switch (touchevent.getAction()) {
+
+            case MotionEvent.ACTION_DOWN:
+                lastX = touchevent.getX();
+                break;
+            case MotionEvent.ACTION_UP:
+                float currentX = touchevent.getX();
+
+                // Handling left to right screen swap.
+                if (lastX < currentX) {
+
+                    // If there aren't any other children, just break.
+                    if (viewFlipper.getDisplayedChild() == 0)
+                        break;
+
+                    // Next screen comes in from left.
+                    viewFlipper.setInAnimation(this, R.anim.slide_in_from_left);
+                    // Current screen goes out from right.
+                    viewFlipper.setOutAnimation(this, R.anim.slide_out_to_right);
+
+                    // Display next screen.
+                    viewFlipper.showNext();
+                }
+
+                // Handling right to left screen swap.
+                if (lastX > currentX) {
+
+                    // If there is a child (to the left), kust break.
+                    if (viewFlipper.getDisplayedChild() == 1)
+                        break;
+
+                    // Next screen comes in from right.
+                    viewFlipper.setInAnimation(this, R.anim.slide_in_from_right);
+                    // Current screen goes out from left.
+                    viewFlipper.setOutAnimation(this, R.anim.slide_out_to_left);
+
+                    // Display previous screen.
+                    viewFlipper.showPrevious();
+                }
+                break;
+        }
+        return false;
     }
 }
